b614a38a2bff3f8e84da4ae0d11370254980adc9
Shashank Tiwari <shanshank69@gmail.com>
2018-05-25 16:03:40 +0530

    Update notes.component.ts

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, transition, animate, query, stagger } from '@angular/animations';
+import { trigger, style, state, keyframes, transition, animate, query, stagger } from '@angular/animations';
 
 @Component({
 	selector: 'app-notes',
@@ -12,41 +12,53 @@ import { trigger, style, transition, animate, query, stagger } from '@angular/an
 					stagger(100, [
 						animate('0.2s', style({ height: '0px', display: 'none' }))
 					])
-				], { optional: true })
+				])
 			])
+		]),
+		trigger('slideLeft', [
+			transition('* => *', animate(100, keyframes([
+					style({ left: '*', offset: 0 }),
+					style({ left: '0', offset: 1 }),
+				])
+			))
 		])
 	]
 })
 export class NotesComponent implements OnInit {
 	itemsNotes: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+	indexNumber = null;
 	constructor() {	}
-
 	ngOnInit() { }
-
 	panend(action, index) {
-    console.log('Pan End');
 		const currentMargin = this.getLeftPosition(action);
-		if (currentMargin > 15) {
-			this.itemsNotes.splice(index, 1);
+		if (currentMargin > 15 || currentMargin < -15) {
+			this.removeElement(index);
 		} else {
-			action.target.style.left = '0px';
+			this.indexNumber = index;
 		}
 	}
 
-	panright(action) {
-    console.log('Pan Start');
+	panmove(action, index) {
 		action.target.style.left = action.deltaX + 'px';
 	}
 
+	alignComplete(event) {
+		event.element.style.left = '0px';
+		this.indexNumber = null;
+	}
+
+	removeElement(index) {
+		this.itemsNotes.splice(index, 1);
+	}
+
 	getLeftPosition(action) {
-		const currentleftPosition = action.target.style.left.match(/\d/g);
+		const currentleftPosition = action.target.style.left.slice(0, -2);
 		if (currentleftPosition !== null) {
 			return (parseInt(
-				currentleftPosition.join('') , 10
+				currentleftPosition, 10
 			) * 100) / window.innerWidth;
 		} else {
 			return 0;
 		}
 	}
-
 }
